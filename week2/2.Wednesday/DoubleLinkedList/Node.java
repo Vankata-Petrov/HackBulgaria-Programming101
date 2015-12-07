618250a380ff0a0237a4a7ad0bb3c0796cd05fed
Vankata-Petrov <vankata434@gmail.com>
2015-12-07 17:45:10 +0200

    Create Node.java

---
+++ week2/2.Wednesday/DoubleLinkedList/Node.java
@@ -0,0 +1,43 @@
+public class Node {
+
+        private int data;
+        private Node next;
+        private Node previous;
+
+        public Node() {
+        }
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node previous,Node next) {
+            this.data = data;
+            this.next = next;
+            this.previous=previous;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+        
+        public Node getPrevious() {
+          return previous;
+        }
+        
+        public void setPrevious(Node previous) {
+          this.previous = previous;
+        }
+  }
