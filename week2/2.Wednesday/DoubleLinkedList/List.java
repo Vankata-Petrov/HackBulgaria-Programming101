677bc163665953f3bb9338b4c8f405761c7b8f6b
Vankata-Petrov <vankata434@gmail.com>
2015-12-07 17:46:35 +0200

    Create List.java

---
+++ week2/2.Wednesday/DoubleLinkedList/List.java
@@ -0,0 +1,20 @@
+public interface List {
+
+    public void add(int value);
+
+    public void insert(int index, int value);
+
+    public void remove();
+
+    public void remove(int index);
+
+    //public void reverse();
+
+    public int getSize();
+
+    public int getHead();
+    
+    public int getTail();
+    
+    public boolean isEmpty();
+}
